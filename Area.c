4d8831341fc9debc9a0a95e4a906312a656f1ea3
JOSE JOAO CAETANO <josejoaocaetano16@gmail.com>
2025-05-02 22:08:17 -0300

    Add files via upload

---
+++ Area.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+	double = a, b, c;
+	double = TriRetangulo, AreaCirculo, AreaTrapezio, AreaQuadrado AreaRetangulo, pi = 3.14159;
+	
+	scanf(" %If %If %If ", &a, &b, &c);
+	
+	TriRetangulo = a * (base + altura);
+	AreaCirculo = c * ( pi );
+	AreaTrapezio = a * b (base) c * (altura);
+	AreaQuadrado = area * b;
+	AreaRetangulo = a * b;
+	
+	printf(" %If %If %If %If %If\n", TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO);
